c177ff7769f22e7c62ec27a420660b3f302c4a4e
grosser <grosser.michael@gmail.com>
2009-05-24 13:10:09 +0200

    graphs edited to better fit into github

--- exmaples/multi_line_graph.rb
+++ exmaples/multi_line_graph.rb
@@ -9,8 +9,8 @@ require 'db_graph/line'
 1000.times{Product.create!(:created_at=>Time.parse('2008-01-01')+rand(1.year),  :updated_at=>Time.parse('2008-01-01')+rand(1.year))}
 500.times{Product.create!(:created_at=>Time.parse('2008-10-01')+rand(2.months), :updated_at=>Time.parse('2008-10-01')+rand(2.months))}
 
-[:days, :weeks, :months].each do |interval|
-  g = DBGraph::Line.new(interval)
+[:weeks, :months].each do |interval|
+  g = DBGraph::Line.new(interval, :size=>'449x400')
   g.add Product, :created_at
   g.add Product, :updated_at
   puts "![#{interval}](#{g.to_url})"
